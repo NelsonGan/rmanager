d363db20e1cd8e44ba7a40cec108cb4227704090
Seejay-01 <seejaymsia2001+github@gmail.com>
2020-12-30 13:14:54 +0800

    Page Template Created

--- inventory.php
+++ inventory.php
@@ -11,28 +11,28 @@
     </div>
     
     <div style="flex: 5;">
-        <div class="topbar">
-            <span>Inventory Record</span>
-        </div>
+        <div class="mainbody">
+            <div class="topbar">
+                <span>Inventory Record</span>
+            </div>
 
-        <div>
-            <center>
-                <table class="itemtable">
-                    <tr>
-                        <th style="width: 20%;">Item Code</th>
-                        <th style="width: 30%;">Item Name</th>
-                        <th style="width: 40%;">Quantity</th>
-                    </tr>
+            <div>
+                <center>
+                    <table class="itemtable">
+                        <tr>
+                            <th style="width: 20%;">Item Code</th>
+                            <th style="width: 30%;">Item Name</th>
+                            <th style="width: 40%;">Quantity</th>
+                        </tr>
 
-                    <tr>
-                        <td>001</td>
-                        <td>Potato</td>
-                        <td><input type="text"></td>
-                    </tr>
-                </table>
-            </center>
-            
+                        <tr>
+                            <td>001</td>
+                            <td>Potato</td>
+                            <td><input type="text"></td>
+                        </tr>
+                    </table>
+                </center>
+            </div>
         </div>
     </div>
-    
 </body>
---
+++ template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <title>Default Title</title>
+    <link href="stylesheets/default.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div style="flex: 1;">
+        <?php include "sidebar.html"; ?>
+    </div>
+    
+    <div style="flex: 5;">
+        <div class="mainbody">
+            <div class="topbar">
+                <span>Default Text</span>
+            </div>
+
+        </div>
+    </div>
+</body>
