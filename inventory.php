81f07aaf6cd7c007589c1147e50a3d1a8351d75b
Seejay-01 <seejaymsia2001+github@gmail.com>
2020-12-30 15:23:10 +0800

    Complete Inventory Page Layout

--- inventory.php
+++ inventory.php
@@ -13,22 +13,48 @@
             <span>Inventory Record</span>
         </div>
 
-        <div>
-            <center>
+        <center>
+            <p style="margin-top: 40px; font-size: 30px;">Inventory Record for December 2020</p>
+
+            <input type="text" id="itemfilter" placeholder="Filter:">
+            <select id="locations" name="locations">
+                <option value="all">All</option>
+                <option value="kitchen">Kitchen</option>
+                <option value="storeroom">Storeroom</option>
+                <option value="fridge">Fridge</option>
+                <option value="counter">Counter</option>
+            </select>
+
+            <div>
                 <table class="itemtable">
                     <tr>
-                        <th style="width: 20%;">Item Code</th>
-                        <th style="width: 30%;">Item Name</th>
+                        <th style="width: 30%;">Item Code</th>
+                        <th style="width: 40%;">Item Name</th>
                         <th style="width: 40%;">Quantity</th>
                     </tr>
 
                     <tr>
                         <td>001</td>
                         <td>Potato</td>
-                        <td><input type="text"></td>
+                        <td><input type="text" class="quantity"></td>
+                    </tr>
+
+                    <tr>
+                        <td>001</td>
+                        <td>Potato</td>
+                        <td><input type="text" class="quantity"></td>
+                    </tr>
+
+                    <tr>
+                        <td>001</td>
+                        <td>Potato</td>
+                        <td><input type="text" class="quantity"></td>
                     </tr>
                 </table>
-            </center>
-        </div>
+
+                <input type="Submit" value="Submit" class="btn" id="submitrecord">
+            </div>
+        </center>
+        
     </div>
 </body>
