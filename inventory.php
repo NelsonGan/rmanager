c737dc094106033c48e42c9abea6d6e5ca09c9e2
Seejay-01 <seejaymsia2001+github@gmail.com>
2020-12-30 13:38:02 +0800

    Fixed sidebar positioning

--- inventory.php
+++ inventory.php
@@ -6,33 +6,29 @@
 </head>
 
 <body>
-    <div style="flex: 1;">
-        <?php include "sidebar.html"; ?>
-    </div>
+    <?php include "sidebar.html"; ?>
     
-    <div style="flex: 5;">
-        <div class="mainbody">
-            <div class="topbar">
-                <span>Inventory Record</span>
-            </div>
+    <div class="mainbody">
+        <div class="topbar">
+            <span>Inventory Record</span>
+        </div>
 
-            <div>
-                <center>
-                    <table class="itemtable">
-                        <tr>
-                            <th style="width: 20%;">Item Code</th>
-                            <th style="width: 30%;">Item Name</th>
-                            <th style="width: 40%;">Quantity</th>
-                        </tr>
+        <div>
+            <center>
+                <table class="itemtable">
+                    <tr>
+                        <th style="width: 20%;">Item Code</th>
+                        <th style="width: 30%;">Item Name</th>
+                        <th style="width: 40%;">Quantity</th>
+                    </tr>
 
-                        <tr>
-                            <td>001</td>
-                            <td>Potato</td>
-                            <td><input type="text"></td>
-                        </tr>
-                    </table>
-                </center>
-            </div>
+                    <tr>
+                        <td>001</td>
+                        <td>Potato</td>
+                        <td><input type="text"></td>
+                    </tr>
+                </table>
+            </center>
         </div>
     </div>
 </body>
