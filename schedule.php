9fd3ab3f9e1e249289171051b038665335b4a722
Seejay-01 <seejaymsia2001+github@gmail.com>
2021-01-04 20:12:40 +0800

    Added schedule page

--- schedule.php
+++ schedule.php
@@ -18,15 +18,165 @@
 
             <table class="timetable">
                 <tr id="HeaderRow">
-                    <td>Monday</td>
-                    <td>Tuesday</td>
-                    <td>Wednesday</td>
-                    <td>Thursday</td>
-                    <td>Friday</td>
-                    <td>Saturday</td>
-                    <td>Sunday</td>
+                    <td width="10%;">Monday <br> <span class="date">(23/1/2020)</span></td>
+                    <td width="10%;">Tuesday <br> <span class="date">(23/1/2020)</span></td>
+                    <td width="10%;">Wednesday <br> <span class="date">(23/1/2020)</span></td>
+                    <td width="10%;">Thursday <br> <span class="date">(23/1/2020)</span></td>
+                    <td width="10%;">Friday <br> <span class="date">(23/1/2020)</span></td>
+                    <td width="10%;">Saturday <br> <span class="date">(23/1/2020)</span></td>
+                    <td width="10%;">Sunday <br> <span class="date">(23/1/2020)</span></td>
+                </tr>
+
+                <tr>
+                    <td id="Monday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                            <button class="removeslot">Remove Slot</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                           
+                        </div>
+
+                        <button class="btn" style="margin-top: 20px;">Add Slot</button>
+                    </td>
+
+                    <td id="Tuesday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+                    </td>
+
+                    <td id="Wednesday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+                    </td>
+
+                    <td id="Thursday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+                    </td>
+
+                    <td id="Friday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+                    </td>
+
+                    <td id="Saturday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+                    </td>
+
+                    <td id="Sunday" class="day">
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+
+                        <div class="slot">
+                            <p><strong>Shift: </strong></p>
+                            <p style="font-size: 18px;">0800 to 1200</p>
+                            <p><strong>Staff Assigned: </strong></p>
+                            <p style="font-size: 18px;">John Smith</p>
+
+                            <button class="addstaff">Manage Staff</button>
+                        </div>
+                    </td>
                 </tr>
             </table>
+
+            <div class="manage">
+                <div class="stafflist">d</div>
+            </div>
         </center>
 
 
