7d14b8f6c3a4d4f6aa3de841e465d6eb4a4e6084
SteveYap <62879069+SteveYap@users.noreply.github.com>
2021-01-05 20:11:11 +0800

    Update Owner.php

--- Owner.php
+++ Owner.php
@@ -2,6 +2,9 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <title>Owner</title>
   <link rel="stylesheet" href="stylesheets/default.css">
   <link rel="stylesheet" href="stylesheets/owner.css">
@@ -15,19 +18,28 @@
     <div class="topbar">
       <span>Sales Reports</span>
     </div>
-    <center>
-    <table class="columns">
+    <div class="container-fluid">
+    <div class="table-responsive">
+    <table class="table">
       <tr>
         <th><div id="barchart"></div>
         <th><div id="curve_chart"></div>
+        <th rowspan="2">
+          <div class="container mx-2 mt-3 bg-white border">
+          <h2>Filter:</h2>
+          Month: <input type="month"><br>
+          Date: <input type="date"><br>
+
+
+        </div>
       </tr>
       <tr>
         <th><div id="piechart"></div>
         <th><div id="combochart"></div>
       </tr>
     </table>
-  </center>
   </div>
+</div>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="javascripts/owner.js"></script>
