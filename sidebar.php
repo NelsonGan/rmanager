a0d0b2d2193fcd511bb367381173a60ec611694b
NelsonG <65579637+NelsonGan@users.noreply.github.com>
2021-01-05 18:19:43 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -16,18 +16,18 @@
                 <li><a href='account.php'>Account</a></li>
             <?php }
             else { ?>
-                <li><a href="checkin.php"><i class="fas fa-home"></i>Attendance</a></li>
-                <li><a href="leaverequestform.php"><i class="fas fa-home"></i>Apply Leave</a></li>
-                <li><a href="table.php"><i class="fas fa-home"></i>Take Order</a></li>
-                <li><a href="inventory.php"><i class="fas fa-home"></i>Manage Inventory</a></li>
-                <li><a href="profile-staff"><i class="fas fa-home"></i>Profile</a></li>
+                <li><a href="checkin.php"><i class="fas fa-user-friends"></i>Attendance</a></li>
+                <li><a href="leaverequestform.php"><i class="fas fa-copy"></i>Apply Leave</a></li>
+                <li><a href="table.php"><i class="fas fa-shopping-cart"></i>Take Order</a></li>
+                <li><a href="inventory.php"><i class="fas fa-warehouse"></i>Manage Inventory</a></li>
+                <li><a href="profile-staff"><i class="fas fa-user"></i>Profile</a></li>
+                <li><a href="#" onclick="openDrop()"><i class="fas fa-map-pin"></i>Dropdown</a></li>
+                <div class="dropdown-container" id="todrop">
+                        <a href="#" >Link 1</a>
+                        <a href="#" >Link 2</a>
+                        <a href="#" >Link 3</a>
+                </div>
             <?php } ?>
-            <li><a href="#" onclick="openDrop()"><i class="fas fa-map-pin"></i>Dropdown</a></li>
-            <div class="dropdown-container" id="todrop">
-                    <a href="#" >Link 1</a>
-                    <a href="#" >Link 2</a>
-                    <a href="#" >Link 3</a>
-            </div>
         </ul> 
     </div>
 </body>
