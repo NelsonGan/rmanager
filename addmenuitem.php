1c8d2e123a246be74a8e44fce7e3d048d8a3c985
SeejayDev <74895766+SeejayDev@users.noreply.github.com>
2021-01-05 18:30:24 +0800

    edited manage menu

---
+++ addmenuitem.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <title>Default Title</title>
+    <link href="stylesheets/default.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php include "sidebar.php"; ?>
+    
+    <div class="mainbody">
+        <div class="topbar">
+            <span>Default Text</span>
+        </div>
+
+        <!-- Write your code here! -->
+
+    </div>
+</body>
