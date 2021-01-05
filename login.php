69e80f3b68055a4012786b92b10b1901cf6221a2
NelsonG <65579637+NelsonGan@users.noreply.github.com>
2021-01-05 17:25:48 +0800

    Modified login and sidebar

--- login.php
+++ login.php
@@ -4,21 +4,23 @@
     <link rel="stylesheet" href="stylesheets/login.css">
 </head>
 <body>
+  <form action="includes/signin.php" method="post">
     <img class="logo" src="images/logo.png">
     <div class="wrapper">
       <div class="input-data">
-        <input type="text" placeholder= "Username" onfocus="this.placeholder=''" onblur="this.placeholder='Username'" required>
+        <input type="email" name="mail" required placeholder= "Username" onfocus="this.placeholder=''" onblur="this.placeholder='Username'" required>
       </div>
     </div>
     <div class="wrapper">
       <div class="input-data">
-        <input type="password" placeholder= "Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'" required>
+        <input type="password" name="pwd" required placeholder= "Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'" required>
       </div>
     </div>
     <center>
       <div class="login-btn">
-        <input type="submit" value="Login">
+        <input type="submit" name="login-submit" value="Login">
       </div>
     </center>
+  </form>
 </body>
 </html>
