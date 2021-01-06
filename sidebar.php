f952e015f12df891de124777d78d063a14b5a207
NelsonG <65579637+NelsonGan@users.noreply.github.com>
2021-01-06 12:31:01 +0800

    Added login logout feature

--- sidebar.php
+++ sidebar.php
@@ -10,16 +10,19 @@
         <img src="images/logo.png" style="height: 20%; margin-bottom: 30px;">
         <ul>
         <?php
-            $_SESSION['role'] == "Full-time Staff";
+            $_SESSION['role'] == "Owner";
             if ($_SESSION['role'] == 'Owner') { ?>
-                <li><a href='cart.php'>Cart</a></li>
-                <li><a href='account.php'>Account</a></li>
+                <li><a href="viewstaff.php"><i class="fas fa-user-friends"></i>Manage Staff</a></li>
+                <li><a href="viewleave.php"><i class="fas fa-copy"></i>Leave Applications</a></li>
+                <li><a href="schedule.php"><i class="fas fa-shopping-cart"></i>Staff Schedule</a></li>
+                <li><a href="salesreport.php"><i class="fas fa-warehouse"></i>Sales Report</a></li>
+                <li><a href="managemenu.php"><i class="fas fa-user"></i>Menu Management</a></li>
             <?php }
             else { ?>
                 <li><a href="checkin.php"><i class="fas fa-user-friends"></i>Attendance</a></li>
                 <li><a href="leaverequestform.php"><i class="fas fa-copy"></i>Apply Leave</a></li>
                 <li><a href="table.php"><i class="fas fa-shopping-cart"></i>Take Order</a></li>
-                <li><a href="inventory.php"><i class="fas fa-warehouse"></i>Manage Inventory</a></li>
+                <li><a href="inventorymainpage.php"><i class="fas fa-warehouse"></i>Manage Inventory</a></li>
                 <li><a href="profile-staff"><i class="fas fa-user"></i>Profile</a></li>
                 <li><a href="#" onclick="openDrop()"><i class="fas fa-map-pin"></i>Dropdown</a></li>
                 <div class="dropdown-container" id="todrop">
@@ -28,6 +31,7 @@
                         <a href="#" >Link 3</a>
                 </div>
             <?php } ?>
+            <li><a href="includes/signout.php"><i class="fas fa-user"></i>Logout</a></li>
         </ul> 
     </div>
 </body>
