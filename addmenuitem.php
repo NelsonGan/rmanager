649618a2d6ae6ed0b37d7eabaac8072824c3c67b
SeejayDev <74895766+SeejayDev@users.noreply.github.com>
2021-01-06 16:13:32 +0800

    edit manage menu/ added add menu

--- addmenuitem.php
+++ addmenuitem.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head>
     <title>Default Title</title>
+    <link rel="stylesheet" href="stylesheets/addmenuitem.css">
     <link href="stylesheets/default.css" rel="stylesheet" type="text/css">
 </head>
 
@@ -9,10 +10,35 @@
     
     <div class="mainbody">
         <div class="topbar">
-            <span>Default Text</span>
+            <span>Add Menu Item</span>
         </div>
+        
+        <div class="container">
+                <div class="content">
+                    <div class="content-left">
+                        <p>Name :</p>
+                        <p>Price :</p>
+                        <p>Type :</p>
+                        <p>Description :</p>
+                        <p class="uploadImageText">Upload image :</p>
+                    </div>
+                    <div class="content-right">
+                        <input type="textbox"><br>
+                        <input type="textbox"><br>
+                        <select id="role" name="role">
+                            <option value="null">Select-one</option>
+                            <option value="Food">Food</option>
+                            <option value="Drinks">Drinks</option>
+                            <option value="Appetizers">Appetizers</option>
+                            <option value="Desserts">Desserts</option>
+                        </select><br>
+                        <textarea rows="4" cols="50" name="comment"></textarea><br>
+                        <input type="textbox" class="uploadImageTextBox"><br>
+                    </div>
+                </div>  
 
-        <!-- Write your code here! -->
-
+                <input type="submit" class="btn" value="Clear" style="margin-top: 10px; height: 35px;"> 
+                <input type="submit" class="btn" value="Add Item" style="margin-top: 10px; height: 35px;"> 
+        </div>  
     </div>
 </body>
