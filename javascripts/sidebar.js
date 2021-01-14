cf69070cea2699e1ceb02f97cc16a5ff57746531
wj <67236659+XyZaBc-alt@users.noreply.github.com>
2021-01-14 13:22:31 +0800

    order in sidebar

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -5,3 +5,11 @@ function openDrop(){
     document.getElementById('todrop').style.display = "flex";
   }
 }
+
+function Dropdown(){
+  if (document.getElementById('drop').style.display == "flex"){
+    document.getElementById('drop').style.display = "none";
+  } else {
+    document.getElementById('drop').style.display = "flex";
+  }
+}
